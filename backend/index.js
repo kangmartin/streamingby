0c7526043f450e88354e536c7aa4913af4c94570
Martin Kang <martin.kang@efrei.net>
2025-10-14 17:30:25 +0200

    Add dotenv and update .gitignore to include .env

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+import axios from "axios"
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+const api_key = process.env.API_KEY
